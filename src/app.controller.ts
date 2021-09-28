884714a870f63c1bd378f1e4081e629d57ca7c5d
Pob <590650@gmail.com>
2021-09-29 02:16:19 +0700

    Copy from nestjs doc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,58 +8,59 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-// import { PostService } from './post.service';
 import { User as UserModel, Post as PostModel } from '@prisma/client';
+import { PostService } from './post.service';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly userService: UserService, // private readonly postService: PostService,
+    private readonly userService: UserService,
+    private readonly postService: PostService,
   ) {}
 
-  // @Get('post/:id')
-  // async getPostById(@Param('id') id: string): Promise<PostModel> {
-  //   return this.postService.post({ id: Number(id) });
-  // }
+  @Get('post/:id')
+  async getPostById(@Param('id') id: string): Promise<PostModel | null> {
+    return this.postService.post({ id: Number(id) });
+  }
 
-  // @Get('feed')
-  // async getPublishedPosts(): Promise<PostModel[]> {
-  //   return this.postService.posts({
-  //     where: { published: true },
-  //   });
-  // }
+  @Get('feed')
+  async getPublishedPosts(): Promise<PostModel[]> {
+    return this.postService.posts({
+      where: { published: true },
+    });
+  }
 
-  // @Get('filtered-posts/:searchString')
-  // async getFilteredPosts(
-  //   @Param('searchString') searchString: string,
-  // ): Promise<PostModel[]> {
-  //   return this.postService.posts({
-  //     where: {
-  //       OR: [
-  //         {
-  //           title: { contains: searchString },
-  //         },
-  //         {
-  //           content: { contains: searchString },
-  //         },
-  //       ],
-  //     },
-  //   });
-  // }
+  @Get('filtered-posts/:searchString')
+  async getFilteredPosts(
+    @Param('searchString') searchString: string,
+  ): Promise<PostModel[]> {
+    return this.postService.posts({
+      where: {
+        OR: [
+          {
+            title: { contains: searchString },
+          },
+          {
+            content: { contains: searchString },
+          },
+        ],
+      },
+    });
+  }
 
-  // @Post('post')
-  // async createDraft(
-  //   @Body() postData: { title: string; content?: string; authorEmail: string },
-  // ): Promise<PostModel> {
-  //   const { title, content, authorEmail } = postData;
-  //   return this.postService.createPost({
-  //     title,
-  //     content,
-  //     author: {
-  //       connect: { email: authorEmail },
-  //     },
-  //   });
-  // }
+  @Post('post')
+  async createDraft(
+    @Body() postData: { title: string; content?: string; authorEmail: string },
+  ): Promise<PostModel> {
+    const { title, content, authorEmail } = postData;
+    return this.postService.createPost({
+      title,
+      content,
+      author: {
+        connect: { email: authorEmail },
+      },
+    });
+  }
 
   @Post('user')
   async signupUser(
@@ -68,21 +69,16 @@ export class AppController {
     return this.userService.createUser(userData);
   }
 
-  @Get('user')
-  async getAllUsers(): Promise<UserModel[]> {
-    return this.userService.users({});
+  @Put('publish/:id')
+  async publishPost(@Param('id') id: string): Promise<PostModel> {
+    return this.postService.updatePost({
+      where: { id: Number(id) },
+      data: { published: true },
+    });
   }
 
-  // @Put('publish/:id')
-  // async publishPost(@Param('id') id: string): Promise<PostModel> {
-  //   return this.postService.updatePost({
-  //     where: { id: Number(id) },
-  //     data: { published: true },
-  //   });
-  // }
-
-  // @Delete('post/:id')
-  // async deletePost(@Param('id') id: string): Promise<PostModel> {
-  //   return this.postService.deletePost({ id: Number(id) });
-  // }
+  @Delete('post/:id')
+  async deletePost(@Param('id') id: string): Promise<PostModel> {
+    return this.postService.deletePost({ id: Number(id) });
+  }
 }
